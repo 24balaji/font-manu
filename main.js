57fcd812d612e6cc91c75ef67ac0a16250393a71
24balaji <99334076+24balaji@users.noreply.github.com>
2022-10-12 18:54:27 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+function preload()
+{
+
+}
+
+function setup()
+{
+canvas = createCanvas(390 , 300);
+canvas.center();
+video = createCapture(VIDEO);
+video.size(350,300);
+}
+
+function draw() 
+{
+
+
+}
+
+
+
+
